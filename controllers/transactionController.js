222e224736dcda7a12904aed5b6c90dbee92e21b
Daniel K Bates <danielbates95@gmail.com>
2020-10-23 11:52:00 -0400

    .

--- controllers/transactionController.js
+++ controllers/transactionController.js
@@ -8,6 +8,7 @@ module.exports = {
     },
     getAllData: function (req, res) {
         db.Transaction 
+            .find()
             .sort({startDate: -1})
             .then(dbModel => res.json(dbModel))
             .catch(err => res.status(422).json(err))
